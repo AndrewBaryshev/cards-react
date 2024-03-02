b9f2dfd0c8bef9875f203823933dff4280e581a1
a.baryshev <a.baryshev@astondevs.ru>
2024-03-03 00:28:11 +0300

    add likes-icon + fix text-price

--- src/components/card/CardComponent.tsx
+++ src/components/card/CardComponent.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import styles from "./cardComponent.module.css";
 import { ICardComponent } from "./type";
+import { Like } from "../../svg";
 
 export const CardComponent: FC<ICardComponent> = ({ data, image, id }) => {
   const handleOnClick = () => {
@@ -12,11 +13,17 @@ export const CardComponent: FC<ICardComponent> = ({ data, image, id }) => {
       <img className={styles.imgClass} src={image} />
       <div className={styles.info}>
         <div className={styles.price}>
-          <label className={styles.currentPrice}>1700</label>
-          <label className={styles.oldPrice}>1700</label>
+          <label className={styles.currentPrice}>1700 ₽</label>
+          <label className={styles.oldPrice}>1700 ₽</label>
         </div>
         <div className={styles.description}>
           <p className={styles.dataText}>{data}</p>
+        </div>
+        <div className={styles.likesContainer}>
+          <Like />
+          <span className={styles.likesNumber}>
+            {Math.floor(Math.random() * 5) + 1}
+          </span>
         </div>
         <button onClick={() => handleOnClick()} className={styles.buttonCart}>
           В корзину
